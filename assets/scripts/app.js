3ba1de193a3f1c86846db06d73282cb9ff8de433
Валерий <grizodubov@contract-center.ru>
2021-01-13 19:26:49 +0300

    Data Types & Constants

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,8 @@
-alert('This works!')
+const dafaultResult = 0
+let currentResult = dafaultResult;
+
+currentResult = currentResult + 10;
+
+let calculationDescription = `${dafaultResult} + 10`;
+
+outputResult(currentResult, calculationDescription);
