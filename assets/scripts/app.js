6b94394761347a08bcb7d48bd9c14beb3cbe5e73
login <grizodubov@yahoo.com>
2021-02-02 17:17:26 +0300

    ok

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,11 +5,36 @@ function getUserNumberInput(){
     return parseInt(userInput.value);
 }
 
+function createAndWrightOutput(operator, resultBeforeCalc, calcNumber){
+
+}
+
 function add() {
   const enteredNumber = getUserNumberInput();
   const calcDescriptions = `${currentResult} + ${enteredNumber}`;
   currentResult = currentResult + enteredNumber;
   outputResult(currentResult, calcDescriptions);
+}
+
+function substract(){
+  const enteredNumber = getUserNumberInput();
+  const calcDescriptions = `${currentResult} - ${enteredNumber}`;
+  currentResult = currentResult - enteredNumber;
+  outputResult(currentResult, calcDescriptions);
+}
+
+function multiply() {
+  const enteredNumber = getUserNumberInput();
+  const calcDescriptions = `${currentResult} + ${enteredNumber}`;
+  currentResult = currentResult + enteredNumber;
+  outputResult(currentResult, calcDescriptions);
+}
+
+function divide() {
+  const enteredNumber = getUserNumberInput();
+  const calcDescriptions = `${currentResult} + ${enteredNumber}`;
+  currentResult = currentResult + enteredNumber;
+  outputResult(currentResult, calcDescriptions);  
 }
 
 addBtn.addEventListener("click", add);
