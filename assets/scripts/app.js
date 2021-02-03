6f2235d4651bb4d644fb48394120b5debc11b521
Valerii Grizodubov <valeriigrizodubov@iMac.local>
2021-02-03 19:27:20 +0300

    imac

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,40 +1,55 @@
 const dafaultResult = 0;
 let currentResult = dafaultResult;
+let logEntries = [];
 
-function getUserNumberInput(){
-    return parseInt(userInput.value);
+// Gets input from the input field
+function getUserNumberInput() {
+  return parseInt(userInput.value);
 }
 
-function createAndWrightOutput(operator, resultBeforeCalc, calcNumber){
-
+// Generates ans wrights calculation log
+function createAndWrightOutput(operator, resultBeforeCalc, calcNumber) {
+  const calcDescriptions = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+  outputResult(currentResult, calcDescriptions); // from vendor file
 }
 
 function add() {
   const enteredNumber = getUserNumberInput();
-  const calcDescriptions = `${currentResult} + ${enteredNumber}`;
-  currentResult = currentResult + enteredNumber;
-  outputResult(currentResult, calcDescriptions);
+  const initialResult = currentResult;
+  currentResult += enteredNumber;
+  createAndWrightOutput("+", initialResult, enteredNumber);
+  const logEntrie = {
+    operation: "ADD",
+    prevResult: initialResult,
+    number: enteredNumber,
+    result: currentResult,
+  };
+  logEntries.push(logEntrie);
+  console.log(logEntries);
 }
 
-function substract(){
+function substract() {
   const enteredNumber = getUserNumberInput();
-  const calcDescriptions = `${currentResult} - ${enteredNumber}`;
-  currentResult = currentResult - enteredNumber;
-  outputResult(currentResult, calcDescriptions);
+  const initialResult = currentResult;
+  currentResult -= enteredNumber;
+  createAndWrightOutput("-", initialResult, enteredNumber);
 }
 
 function multiply() {
   const enteredNumber = getUserNumberInput();
-  const calcDescriptions = `${currentResult} + ${enteredNumber}`;
-  currentResult = currentResult + enteredNumber;
-  outputResult(currentResult, calcDescriptions);
+  const initialResult = currentResult;
+  currentResult *= enteredNumber;
+  createAndWrightOutput("*", initialResult, enteredNumber);
 }
 
 function divide() {
   const enteredNumber = getUserNumberInput();
-  const calcDescriptions = `${currentResult} + ${enteredNumber}`;
-  currentResult = currentResult + enteredNumber;
-  outputResult(currentResult, calcDescriptions);  
+  const initialResult = currentResult;
+  currentResult /= enteredNumber;
+  createAndWrightOutput("/", initialResult, enteredNumber);
 }
 
 addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", substract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
